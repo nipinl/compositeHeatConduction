2c799efef53ff225630ecddaee12ab5dc8c21563
Nipin L <nipinl@gmail.com>
2021-07-06 13:56:39 +0530

    single shell data output and plotting done

--- compositeCylHT/plotIt.py
+++ compositeCylHT/plotIt.py
@@ -16,23 +16,23 @@ print(geom)
 x = np.linspace(0, 4, 3)
 y = np.linspace(0, 2, 5)
 [X, Y] = np.meshgrid(x,y)
-colorMapEnable = 0 
-for n in range(times-1):
+enableColorBar=0
+for n in range(times):
     fp = open("temperature","r+")
     for i, line in enumerate(fp):
         if(i>n*5 and i < (n+1)*5+1):
             temp.append(line.split())
             print(i)
-
     #fig, ax = plt.subplots(1, 1)
     plt.contourf(X,Y,temp)
-    """ ax.set_title('Filled Contour Plot')
-    ax.set_xlabel('feature_x')
-    ax.set_ylabel('feature_y') """
-    if(colorMapEnable==0):
+    """ plt.set_title('Filled Contour Plot')
+    plt.set_xlabel('feature_x')
+    plt.set_ylabel('feature_y') """
+    if enableColorBar==0:
+        enableColorBar=1
         plt.colorbar()
-    colorMapEnable=1
     plt.savefig(str(n).zfill(5) )
     temp.clear()
     fp.close()
-
+#print(temp)
+#temp.clear()
